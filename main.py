d57f1caa614414b906142d608b9e359951f61e2c
Mystyk <mateuszr@zampub.rzeszow.pl>
2022-12-08 13:05:06 +0100

    Added requirements file

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from keras import Sequential
 import pathlib
 
 if __name__ == '__main__':
-
     dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
     data_dir = tf.keras.utils.get_file('flower_photos', origin=dataset_url, untar=True)
     data_dir = pathlib.Path(data_dir)
