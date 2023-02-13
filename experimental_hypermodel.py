cf2980da562acd957e81bbf50f5b29edc7309da4
Jan Kryński <117764690+JanKrynski@users.noreply.github.com>
2023-02-13 16:50:42 +0100

    Dodałem warstwy przetwarzające obraz

--- experimental_hypermodel.py
+++ experimental_hypermodel.py
@@ -1,22 +1,24 @@
-import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
 from keras import layers
-from keras import Sequential
 import pathlib
-import random
 import keras_tuner as kt
-#kod wymaga jeszcze poprawek bo nie dostajemy zbyt dobrych wyników
 def model_builder(hp):
   model = keras.Sequential()
-  model.add(keras.layers.Flatten(input_shape=(img_height, img_width, 3)))
+
 
   # Tune the number of units in the first Dense layer
   # Choose an optimal value between 32-512
   hp_units = hp.Int('units', min_value=32, max_value=512, step=32)
   hp_units2 = hp.Int('units2', min_value=16, max_value=256, step=16)
   hp_units3 = hp.Int('units3', min_value=8, max_value=128, step=8)
-  model.add(keras.layers.Flatten(input_shape=(img_height, img_width, 3)))
+  model.add(keras.layers.Conv2D(filters=16, kernel_size=(7, 7), padding='same', activation='relu'))
+  model.add(keras.layers.MaxPooling2D())
+  model.add(keras.layers.Conv2D(filters=32, kernel_size=(5, 5), padding='same', activation='relu'))
+  model.add(keras.layers.MaxPooling2D())
+  model.add(keras.layers.Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu'))
+  model.add(keras.layers.MaxPooling2D())
+  model.add(keras.layers.Flatten(input_shape=(180, 180, 3)))
   model.add(keras.layers.Dense(units=hp_units, activation='relu'))
   model.add(keras.layers.Dense(units=hp_units2, activation='relu'))
   model.add(keras.layers.Dense(units=hp_units3, activation='relu'))
