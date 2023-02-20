0c634d7ae58ffc6e262cb65e4046f46459d8f9dc
Przemek Kubas <kubasprzemek9@gmail.com>
2023-02-20 19:32:26 +0100

    1. Make function that reads data and divides it into three sets 2. Create 3 different function to get data, with different degrees of augmentation

--- main.py
+++ main.py
@@ -1,37 +1,127 @@
-import matplotlib.pyplot as plt
 import tensorflow as tf
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
 
 from keras import layers
 from keras import Sequential
 import pathlib
 
+
+def split_data(data_set, train_percent, val_percent, test_percent):
+
+    data_set = data_set.shuffle(buffer_size=len(data_set), reshuffle_each_iteration=False)
+
+    train_size = int(len(data_set)*train_percent)
+    val_size = int(len(data_set)*val_percent)
+    test_size = int(len(data_set)*test_percent)
+
+    train_ds = data_set.take(train_size)
+    val_ds = data_set.skip(train_size).take(val_size)
+    test_ds = data_set.skip(train_size+val_size).take(test_size)
+
+    return train_ds, val_ds, test_ds
+
+
+def image_rot_on_dataset(image, label):
+    image = tf.image.rot90(image)
+    return image, label
+
+
+def random_brightness(image, label):
+    augment_image = tf.image.random_brightness(image, 1.0)
+    return augment_image, label
+
+
+def random_hue(image, label):
+    hue_image = tf.image.random_hue(image, 0.5)
+    return hue_image, label
+
+
+def random_saturation(image, label):
+    saturated_image = tf.image.random_saturation(image, 0, 20)
+    return saturated_image, label
+
+
+def rgb_to_gray_scale(image, label):
+    gray_scale_image = tf.image.rgb_to_grayscale(image)
+    return gray_scale_image, label
+
+
+def prepare(ds, shuffle=False, rot=False, rgb=False, brightness=False, saturation=False, hue=False):
+
+    if shuffle:
+        ds = ds.shuffle(1000)
+
+    if saturation:
+        ds = (
+            ds.map(random_saturation, num_parallel_calls=AUTOTUNE)
+            .batch(batch_size)
+            .prefetch(AUTOTUNE)
+        )
+
+    if brightness:
+        ds = (
+            ds.map(random_brightness, num_parallel_calls=AUTOTUNE)
+            .batch(batch_size)
+            .prefetch(AUTOTUNE)
+        )
+
+    if rgb:
+        ds = (
+            ds.map(rgb_to_gray_scale, num_parallel_calls=AUTOTUNE)
+            .batch(batch_size)
+            .prefetch(AUTOTUNE)
+        )
+
+    if rot:
+        ds = (
+            ds.map(image_rot_on_dataset, num_parallel_calls=AUTOTUNE)
+            .batch(batch_size)
+            .prefetch(AUTOTUNE)
+        )
+
+    if hue:
+        ds = (
+            ds.map(random_hue, num_parallel_calls=AUTOTUNE)
+            .batch(batch_size)
+            .prefetch(AUTOTUNE)
+        )
+
+    return ds.prefetch(buffer_size=AUTOTUNE)
+
+
 if __name__ == '__main__':
 
-    path = "C:/Users/Lenovo/Downloads/flower_photos.tgz" #the path where the dataset is located
+    path = "C:/Users/Lenovo/Downloads/flower_photos.tgz"
     data_dir = tf.keras.utils.get_file('flower_photos', origin=path, untar=True)
     data_dir = pathlib.Path(data_dir)
 
     batch_size = 32
     img_height = 180
     img_width = 180
-    train_ds = tf.keras.utils.image_dataset_from_directory(
+
+    data = tf.keras.utils.image_dataset_from_directory(
         data_dir,
-        validation_split=0.2,
-        subset="training",
         seed=123,
-        image_size=(img_height, img_width),
-        batch_size=batch_size)
-    val_ds = tf.keras.utils.image_dataset_from_directory(
-        data_dir,
-        validation_split=0.2,
-        subset="validation",
-        seed=123,
-        image_size=(img_height, img_width),
-        batch_size=batch_size)
-    class_names = train_ds.class_names
+        batch_size=batch_size,
+        image_size=(img_height, img_width))
+
+    train_ds, val_ds, test_ds = split_data(data, 0.7, 0.2, 0.1)
+
+    # wyciągnięcie jednego elementu ze zbioru train
+    train_one_example = train_ds.take(1)
+
+    # stworzenie nowego zbioru danych z jednym elementem
+    one_example_dataset = tf.data.Dataset.from_tensors(train_one_example)
 
     AUTOTUNE = tf.data.AUTOTUNE
 
+    ds = prepare(one_example_dataset, shuffle=False, rot=False, rgb=True, brightness=False, saturation=False, hue=False)
+
+    class_names = train_ds.class_names
     train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
     val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
