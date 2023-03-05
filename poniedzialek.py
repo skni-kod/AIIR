49d34d249dd11cc22537792df05395e6bc529a1b
Jan Kryński <117764690+JanKrynski@users.noreply.github.com>
2023-03-05 21:40:57 +0100

    Dodano zapis pliku w formacie h5

--- poniedzialek.py
+++ poniedzialek.py
@@ -136,5 +136,5 @@ if __name__ == '__main__':
                         epochs=epochs
                     )
 
-                    model.save('model ' + str(counter))
+                    model.save('model ' + str(counter)+'.h5')
                     counter += 1
